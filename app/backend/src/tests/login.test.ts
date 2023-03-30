e27f47b205beed6e56fcfde0c06dc5a3a687725a
lucascbb <lucasbaroquello@hotmail.com>
2023-03-30 18:50:52 -0300

    testes

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,4 +1,3 @@
-// import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
@@ -19,5 +18,36 @@ describe('Usando o método POST em /login', function () {
 
     expect(response.status).to.be.equal(400);
     expect(response.body).to.have.property("message", 'All fields must be filled');
+  });
+  it('Deve retornar uma mensagem de erro caso campo email seja invalido', async function () {
+    const response = await chai.request(app)
+      .post('/login')
+      .send({
+        email: 'userexample.com',
+        password: '123456'
+      });
+
+    expect(response.status).to.be.equal(401);
+    expect(response.body).to.have.property("message", 'Invalid email or password');
+  });
+  it('Deve retornar uma mensagem de erro caso campo senha seja invalido', async function () {
+    const response = await chai.request(app)
+      .post('/login')
+      .send({
+        email: 'user@trybe.com',
+        password: '0'
+      });
+    expect(response.status).to.be.equal(401);
+    expect(response.body).to.have.property("message", 'Invalid email or password');
+  });
+  it('Deve retornar uma mensagem de erro caso campo email nao tenha no banco', async function () {
+    const response = await chai.request(app)
+      .post('/login')
+      .send({
+        email: 'user@trybe.com',
+        password: '123456'
+      });
+    expect(response.status).to.be.equal(401);
+    expect(response.body).to.have.property("message", 'Invalid email or password');
   });
 });
--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,4 +1,4 @@
-import * as sinon from 'sinon';
+// import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');

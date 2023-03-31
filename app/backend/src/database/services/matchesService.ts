ed15c7d812357280f17f105da42b294e554cee87
lucascbb <lucasbaroquello@hotmail.com>
2023-03-31 15:50:34 -0300

    req 15

--- app/backend/src/database/services/matchesService.ts
+++ app/backend/src/database/services/matchesService.ts
@@ -1,12 +1,27 @@
 import { ModelStatic } from 'sequelize';
 import IMatches from '../interface/IMatches';
 import Matches from '../models/MatchesModel';
+import Teams from '../models/TeamsModel';
 
 export default class MatchesService {
   constructor(private matchesModel:ModelStatic<Matches>) {}
 
   async getAllMatches(): Promise<IMatches[]> {
-    const matches = await this.matchesModel.findAll();
+    const matches = await this.matchesModel.findAll({
+      include: [{
+        model: Teams,
+        as: 'homeTeam',
+        required: true,
+        attributes: ['teamName'],
+      },
+      {
+        model: Teams,
+        as: 'awayTeam',
+        required: true,
+        attributes: ['teamName'],
+      },
+      ],
+    });
     return matches as IMatches[];
   }
 }

b61c8eb70fb53d251799f38a234344d6822a0d5e
lucascbb <lucasbaroquello@hotmail.com>
2023-03-31 00:35:31 -0300

    req 12

--- app/backend/src/database/validations/tokenMiddleware.ts
+++ app/backend/src/database/validations/tokenMiddleware.ts
@@ -5,8 +5,8 @@ function meuMiddleware(req: Request, res: Response, next: NextFunction): any {
   const { authorization } = req.headers;
 
   if (!authorization) { return res.status(401).json({ message: 'Token not found' }); }
+
   const validate = validateToken(authorization);
-  console.log(validate);
   if (!validate) { return res.status(401).json({ message: 'Token must be a valid token' }); }
 
   next();

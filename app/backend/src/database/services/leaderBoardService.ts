ee0b83b3937c0abb24ae03610f554f8b58e46c1b
lucascbb <lucasbaroquello@hotmail.com>
2023-04-03 17:27:53 -0300

    req 23

--- app/backend/src/database/services/leaderBoardService.ts
+++ app/backend/src/database/services/leaderBoardService.ts
@@ -1,32 +1,32 @@
 import { ModelStatic, Sequelize } from 'sequelize';
-import IMatches from '../interface/IMatches';
+// import IMatches from '../interface/IMatches';
 import Matches from '../models/MatchesModel';
 import Teams from '../models/TeamsModel';
 import query from '../../utils/queries';
 
 export default class LeaderBoardService {
   constructor(private leaderModel:ModelStatic<Matches>) {}
+
   async getLeaderBoard(): Promise<any> {
-    const lbH = await this.leaderModel.findAll({
-      where: { homeTeamId: 4, inProgress: 0 },
-      include: [{ model: Teams, as: 'homeTeam', attributes: { exclude: ['teamName', 'id'] } }],
-      attributes: [
-        [Sequelize.col('homeTeam.team_name'), 'name'],
-        [Sequelize.fn('COUNT', Sequelize.col('home_team_id')), 'totalGames'],
-        [Sequelize.literal(query.string1), 'pontosTotais'],
-        [Sequelize.literal(query.string2), 'totalVictories'],
-        [Sequelize.literal(query.string3), 'totalLosses'],
-        [Sequelize.literal(query.string4), 'totalDraws'],
-        [Sequelize.literal(query.string5), 'goalsFavor'],
-        [Sequelize.literal(query.string6), 'goalsOwn'],
-      ],
-      group: ['home_team_id', 'name'],
-    });
-    return lbH as any;
+    const resultLeaderBoard = await Promise.all(query.ids.map(async (ele) => {
+      const lbH = await this.leaderModel.findAll({
+        where: { homeTeamId: ele, inProgress: 0 },
+        include: [{ model: Teams, as: 'homeTeam', attributes: { exclude: ['teamName', 'id'] } }],
+        attributes: [[Sequelize.col('homeTeam.team_name'), 'name'],
+          [Sequelize.literal(query.string1), 'totalPoints'],
+          [Sequelize.fn('COUNT', Sequelize.col('home_team_id')), 'totalGames'],
+          [Sequelize.literal(query.string2), 'totalVictories'],
+          [Sequelize.literal(query.string4), 'totalDraws'],
+          [Sequelize.literal(query.string3), 'totalLosses'],
+          [Sequelize.literal(query.string5), 'goalsFavor'],
+          [Sequelize.literal(query.string6), 'goalsOwn']],
+        group: ['home_team_id', 'name'],
+      }); return lbH as any;
+    }));
+    const resultArr = resultLeaderBoard.flatMap(([obj]) => obj);
+    return resultArr;
   }
 }
-
-const leaderBoardService = new LeaderBoardService(Matches);
-leaderBoardService.getLeaderBoard();
-
+// const leaderBoardService = new LeaderBoardService(Matches);
+// leaderBoardService.getLeaderBoard();
 // ts-node src/database/services/leaderBoardService.ts
--- app/backend/src/utils/queries.ts
+++ app/backend/src/utils/queries.ts
@@ -12,6 +12,8 @@ const string5 = 'SUM(CASE WHEN home_team_goals > 0 THEN home_team_goals ELSE 0 E
 
 const string6 = 'SUM(CASE WHEN away_team_goals > 0 THEN away_team_goals ELSE 0 END)';
 
-const query = { string1, string2, string3, string4, string5, string6 };
+const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+const query = { string1, string2, string3, string4, string5, string6, ids };
 
 export default query;

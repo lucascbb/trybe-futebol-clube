0bfbda089866df2165854dd9da883547b3a02010
lucascbb <lucasbaroquello@hotmail.com>
2023-04-03 15:10:18 -0300

    req 23

--- app/backend/src/database/services/leaderBoardService.ts
+++ app/backend/src/database/services/leaderBoardService.ts
@@ -1,4 +1,5 @@
-import { ModelStatic, Op } from 'sequelize';
+/* eslint-disable max-len */
+import { ModelStatic, Sequelize } from 'sequelize';
 import IMatches from '../interface/IMatches';
 import Matches from '../models/MatchesModel';
 import Teams from '../models/TeamsModel';
@@ -6,42 +7,27 @@ import Teams from '../models/TeamsModel';
 export default class LeaderBoardService {
   constructor(private leaderModel:ModelStatic<Matches>) {}
 
-  async getLeaderBoard(): Promise<IMatches[]> {
-    const array = [1, 2, 3, 4, 5];
-    array.forEach((ele, i) => {
-      console.log(ele, i);
+  async getLeaderBoard(): Promise<any> {
+    const lbH = await this.leaderModel.findAll({
+      where: { homeTeamId: 4, inProgress: 0 },
+      include: [{ model: Teams, as: 'homeTeam', required: true, attributes: { exclude: ['teamName', 'id'] } }],
+      attributes: [
+        [Sequelize.col('homeTeam.team_name'), 'name'],
+        [Sequelize.fn('COUNT', Sequelize.col('home_team_id')), 'totalGames'],
+        [Sequelize.literal('SUM(CASE WHEN home_team_goals > away_team_goals THEN 3 WHEN home_team_goals = away_team_goals THEN 1 ELSE 0 END)'), 'pontosTotais'],
+        [Sequelize.literal('SUM(CASE WHEN home_team_goals > away_team_goals THEN 1 ELSE 0 END)'), 'totalVictories'],
+        [Sequelize.literal('SUM(CASE WHEN home_team_goals < away_team_goals THEN 1 ELSE 0 END)'), 'totalLosses'],
+        [Sequelize.literal('SUM(CASE WHEN home_team_goals = away_team_goals THEN 1 ELSE 0 END)'), 'totalDraws'],
+        [Sequelize.literal('SUM(CASE WHEN home_team_goals > 0 THEN home_team_goals ELSE 0 END)'), 'goalsFavor'],
+        [Sequelize.literal('SUM(CASE WHEN away_team_goals > 0 THEN away_team_goals ELSE 0 END)'), 'goalsOwn'],
+      ],
+      group: ['home_team_id', 'name'],
     });
-
-    const leaderBoardH = await this.leaderModel.findAll({
-      where: { homeTeamId: 1 },
-      include: [{ model: Teams,
-        as: 'homeTeam',
-        required: true,
-        attributes: ['teamName'] }],
-    });
-
-    const obj = { name: leaderBoardH[0].homeTeam.teamName };
-
-    console.log(obj);
-    return leaderBoardH as IMatches[];
+    return lbH as any;
   }
 }
 
 const leaderBoardService = new LeaderBoardService(Matches);
 leaderBoardService.getLeaderBoard();
 
-// getLeaderBoard().then((ele) => console.log(ele));
-// codigo: ts-node src/models/login.model.ts
-
-// {
-//   "name": "Corinthians",
-//   "totalPoints": 12,
-//   "totalGames": 5,
-//   "totalVictories": 4,
-//   "totalDraws": 0,
-//   "totalLosses": 1,
-//   "goalsFavor": 12,
-//   "goalsOwn": 3,
-//   "goalsBalance": 9,
-//   "efficiency": 80
-// }
+// ts-node src/database/services/leaderBoardService.ts

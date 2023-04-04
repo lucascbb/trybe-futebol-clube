b16c70ea75f3962a10c479a37717e2b7a17189f8
lucascbb <lucasbaroquello@hotmail.com>
2023-04-03 22:35:27 -0300

    fix type token

--- app/backend/src/utils/token.validate.ts
+++ app/backend/src/utils/token.validate.ts
@@ -6,7 +6,7 @@ const jwtConfig:object = { expiresIn: '7d', algorithm: 'HS256' };
 
 export const newToken = (id: number, role: string) => jwt.sign({ id, role }, secret, jwtConfig);
 
-export const validateToken = (token: string | any) => {
+export const validateToken = (token: string) => {
   try {
     return jwt.verify(token, secret);
   } catch (error) { return false; }
